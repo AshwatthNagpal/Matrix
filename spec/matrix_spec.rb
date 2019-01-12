1739d328cf10870d119f71e6164c733a62723300
Ashwatth Nagpal <ashwatthnagpal21@gmail.com>
2019-01-12 18:21:09 +0530

    Extract row 2 from matrix string (9 8 7\n19 18 17)

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -8,4 +8,8 @@ describe "Matrix" do
     mat_obj = Matrix.new("9 7\n8 6")
     expect(mat_obj.matrix_rows(0)).to eq [9, 7]
   end
+  it "test extract row \"9 8 7\n19 18 17\"" do
+    mat_obj = Matrix.new("9 8 7\n19 18 17")
+    expect(mat_obj.matrix_rows(1)).to eq [19, 18, 17]
+  end
 end
